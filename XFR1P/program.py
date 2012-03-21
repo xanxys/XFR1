62d37f1acfa8426c5400e47dab2200dbd64648fa
xanxys <xanxys@gmail.com>
2012-03-21 22:27:31 +0900

    reflecting XFR1P and XFR1 onto Programmer and Ring

--- XFR1P/program.py
+++ XFR1P/program.py
@@ -5,135 +5,137 @@ import argparse
 import time
 import logging
 
-# real low level communication (serial port)
-
-# protocol-level
-def enter_debug(ser):
-    print('entering debug mode')
-    ser.write(bytes('d\r\n','UTF-8'))
-    logging.debug(ser.readline())
-
-def enter_normal(ser):
-    print('entering normal mode')
-    ser.write(bytes('n\r\n','UTF-8'))
-    logging.debug(ser.readline())
-
-def get_power(ser):
-    print('checking power')
-    print('sending request')
-    ser.write(bytes('s02\r\n','UTF-8'))
-    logging.debug(ser.readline())
+# representing real programmer (arduino) interface
+# bit like RPC over serial
+class Programmer:
+    def __init__(self,serial_path):
+        ser=serial.Serial(serial_path,19200)
+        logging.info('opened %s'%ser)
+        self.serial=ser
     
-    print('waiting response')
-    ser.write(bytes('r0a\r\n','UTF-8'))
-    r=ser.readline()
-    logging.debug(r)
-    rs=r.decode('ASCII')
-    if rs[0]=='-':
-        v=int(rs[1:],16)
+    # low level function
+    def _send(self,command):
+        logging.debug('programmer<%s'%command)
+        self.serial.write(bytes(command+'\r\n','ASCII'))
+    
+    def _receive(self):
+        while True:
+            s=self.serial.readline().decode('ASCII').rstrip()
+            if s[0]=='#':
+                logging.debug('programmer:%s'%s)
+            elif s[0]=='-':
+                logging.debug('programmer:%s'%s)
+                return s[1:]
+            elif s[0]=='!':
+                raise IOError('programmer failed to complete the command for some reason')
+            else:
+                raise NotImplementedError(
+                    'unknown status %s. Maybe firmware and driver code is not in sync'%s)
+        
+    # exposed methods
+    def version(self):
+        self._send('v')
+        self._receive()
+    
+    def enter_debug(self):
+        self._send('d')
+        self._receive()
+    
+    def enter_normal(self):
+        self._send('n')
+        self._receive()
+    
+    def send_byte(self,data):
+        self._send('s%02x'%data)
+        self._receive()
+    
+    def recv_byte(self,timeout):
+        self._send('r%02x'%int(timeout))
+        return int(self._receive()[0:2],16)
+
+
+
+# expose ring(XFR1) debug mode functionality via Programmer
+class Ring(Programmer):
+    def get_power(self):
+        print('checking power')
+        print('sending request')
+        self.send_byte(2)
+        
+        print('waiting response')
+        v=self.recv_byte(10)
         print('Vcc=%.1f V'%(1.1/(v/256)))
 
-def read_buffer(ser,addr):
-    addr=int(addr,16)
-    
-    print('reading buffer offset 0x%02x'%addr)
-    ser.write(bytes('s00\r\n','UTF-8'))
-    logging.debug(ser.readline())
-#    ser.write(bytes('s%02x\r\n'%((addr>>8)&0xff),'UTF-8'))
-#    logging.debug(ser.readline())
-    ser.write(bytes('s%02x\r\n'%(addr&0xff),'UTF-8'))
-    logging.debug(ser.readline())
-    
-    print('waiting response')
-    ser.write(bytes('r0a\r\n','UTF-8'))
-    r=ser.readline()
-    logging.debug(r)
-    rhex=r.decode('ASCII')[1:3]
-    print(rhex)
-    return int(rhex,16)
+    def read_buffer(self,addr):
+        addr=int(addr,16)
+        
+        print('reading buffer offset 0x%02x'%addr)
+        self.send_byte(0)
+        self.send_byte(addr)
+        
+        print('waiting response')
+        v=self.recv_byte(10)
+        print('%02x'%v)
 
 
-def write_buffer(ser,addr,data):
-    addr=int(addr,16)
-    data=int(data,16)
-    
-    print('writing buffer offset 0x%02x'%addr)
-    ser.write(bytes('s01\r\n','UTF-8'))
-    logging.debug(ser.readline())
-    ser.write(bytes('s%02x\r\n'%(addr&0xff),'UTF-8'))
-    logging.debug(ser.readline())
-    ser.write(bytes('s%02x\r\n'%(data&0xff),'UTF-8'))
-    logging.debug(ser.readline())
-    
-    print('waiting response')
-    ser.write(bytes('r0a\r\n','UTF-8'))
-    r=ser.readline()
-    logging.debug(r)
-    rhex=r.decode('ASCII')[1:3]
+    def write_buffer(self,addr,data):
+        addr=int(addr,16)
+        data=int(data,16)
+        
+        print('writing buffer offset 0x%02x'%addr)
+        self.send_byte(1)
+        self.send_byte(addr)
+        self.send_byte(data)
+        
+        print('waiting response')
+        self.recv_byte(10)
 
-def read_page(ser,addr):
-    addr=int(addr,16)
-    
-    print('reading page 0x%04x to buffer'%addr)
-    ser.write(bytes('s04\r\n','UTF-8'))
-    logging.debug(ser.readline())
-    ser.write(bytes('s%02x\r\n'%((addr>>8)&0xff),'UTF-8'))
-    logging.debug(ser.readline())
-    ser.write(bytes('s%02x\r\n'%(addr&0xff),'UTF-8'))
-    logging.debug(ser.readline())
-    
-    print('waiting response')
-    ser.write(bytes('r0a\r\n','UTF-8'))
-    r=ser.readline()
-    logging.debug(r)
-    rhex=r.decode('ASCII')[1:3]
+    def read_page(self,addr):
+        addr=int(addr,16)
+        
+        print('reading page 0x%04x to buffer'%addr)
+        self.send_byte(4)
+        self.send_byte((addr>>8)&0xff)
+        self.send_byte(addr&0xff)
+        
+        print('waiting response')
+        self.recv_byte(10)
 
-def write_page(ser,addr):
-    addr=int(addr,16)
-    
-    print('writing page 0x%04x from buffer'%addr)
-    ser.write(bytes('s05\r\n','UTF-8'))
-    logging.debug(ser.readline())
-    ser.write(bytes('s%02x\r\n'%((addr>>8)&0xff),'UTF-8'))
-    logging.debug(ser.readline())
-    ser.write(bytes('s%02x\r\n'%(addr&0xff),'UTF-8'))
-    logging.debug(ser.readline())
-    
-    print('waiting response')
-    ser.write(bytes('r0a\r\n','UTF-8'))
-    r=ser.readline()
-    logging.debug(r)
-    rhex=r.decode('ASCII')[1:3]
+    def write_page(self,addr):
+        addr=int(addr,16)
+        
+        print('writing page 0x%04x from buffer'%addr)
+        self.send_byte(5)
+        self.send_byte((addr>>8)&0xff)
+        self.send_byte(addr&0xff)
 
-def hash_buffer(ser):
-    print('hasing buffer')
-    ser.write(bytes('s03\r\n','UTF-8'))
-    logging.debug(ser.readline())
-    
-    print('waiting response')
-    ser.write(bytes('r0a\r\n','UTF-8'))
-    r=ser.readline()
-    logging.debug(r)
-    rhex=r.decode('ASCII')[1:3]
-    print(rhex)
+        print('waiting response')
+        self.recv_byte(10)
+
+    def hash_buffer(self):
+        print('hasing buffer')
+        self.send_byte(3)
+        
+        print('waiting response')
+        v=self.recv_byte(10)
+        print('%02x'%v)
 
 
 # high-level function
 
 
 def proc(args,fn):
-    ser=serial.Serial(args.port,19200)
-    logging.info('opened %s'%ser)
+    prog=Ring(args.port)
 
     if not args.noreset_enter:
-        enter_debug(ser)
+        prog.enter_debug()
         time.sleep(0.5)
     
-    fn(ser)
+    fn(prog)
     
     if not args.noreset_leave:
         time.sleep(0.5)
-        enter_normal(ser)
+        prog.enter_normal()
 
 def main():
     ps=argparse.ArgumentParser(description='optical programmer')
@@ -149,19 +151,19 @@ def main():
     args=ps.parse_args()
     
     if args.command=='status':
-        proc(args,get_power)
+        proc(args,lambda p:p.get_power())
     # buffer manipulation
     elif args.command=='read':
-        proc(args,lambda ser:read_buffer(ser,args.addr))
+        proc(args,lambda p:p.read_buffer(args.addr))
     elif args.command=='write':
-        proc(args,lambda ser:write_buffer(ser,args.addr,args.data))
+        proc(args,lambda p:p.write_buffer(args.addr,args.data))
     elif args.command=='hash':
-        proc(args,hash_buffer)
+        proc(args,lambda p:p.hash_buffer())
     # page manipulation
     elif args.command=='read_page':
-        proc(args,lambda ser:read_page(ser,args.addr))
+        proc(args,lambda p:p.read_page(args.addr))
     elif args.command=='write_page':
-        proc(args,lambda ser:write_page(ser,args.addr))
+        proc(args,lambda p:p.write_page(args.addr))
 
 
 if __name__=='__main__':
